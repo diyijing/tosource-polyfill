3722471380ceceaa1aa45d1783d3d09c1f67c53c
Jesús Leganés Combarro 'piranna <piranna@gmail.com>
2015-08-06 12:18:13 +0200

    Reorganized code

--- index.js
+++ index.js
@@ -1,49 +1,25 @@
+const DEFAULT_INDENT = '  '
+const KEYWORD_REGEXP = new RegExp
+('^('+
+  'abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|'+
+  'default|delete|do|double|else|enum|export|extends|false|final|finally|'+
+  'float|for|function|goto|if|implements|import|in|instanceof|int|interface|'+
+  'long|native|new|null|package|private|protected|public|return|short|static|'+
+  'super|switch|synchronized|this|throw|throws|transient|true|try|typeof|'+
+  'undefined|var|void|volatile|while|with'+
+')$')
+
+
+var _filter
+var _indent = DEFAULT_INDENT
+
+var seen = []
+
+
 function applyToSource(prototype, func)
 {
   if(prototype.toSource === undefined)
     Object.defineProperty(prototype, 'toSource', {value: func})
-}
-
-
-// Objects where toSource is equal to toString
-[Boolean, Function, Number, RegExp].forEach(function(constructor)
-{
-  var prototype = constructor.prototype
-
-  applyToSource(prototype, prototype.toString)
-});
-
-
-applyToSource(Date.prototype, function()
-{
-  return 'new Date('+this.getTime()+')'
-})
-
-applyToSource(String.prototype, function()
-{
-  return JSON.stringify(this)
-})
-
-applyToSource(Math, function()
-{
-  return 'Math'
-})
-
-
-// Recursive objects
-const DEFAULT_INDENT = '  '
-
-var _filter
-var _indent
-var seen = []
-
-
-function join(elements) {
-  var offset = Array(seen.length+1).join(_indent)
-
-  return _indent.slice(1)
-       + elements.join(','+(_indent&&'\n')+offset)
-       + (_indent&&' ');
 }
 
 function fill(keys, func, thisArg)
@@ -61,6 +37,78 @@ function fill(keys, func, thisArg)
   return items
 }
 
+function getIndent(indent)
+{
+  switch(typeof indent)
+  {
+    case 'boolean':   return indent ? DEFAULT_INDENT : ''
+    case 'number':    return Array(indent+1).join(' ')
+    case 'string':    return indent
+    case 'undefined': return DEFAULT_INDENT
+  }
+
+  if(indent === null) return ''
+
+  throw SyntaxError('Invalid indent: '+indent)
+}
+
+function join(elements)
+{
+  var offset = Array(seen.length+1).join(_indent)
+
+  return _indent.slice(1)
+       + elements.join(','+(_indent&&'\n')+offset)
+       + (_indent&&' ');
+}
+
+function legalKey(string)
+{
+  return /^[a-z_$][0-9a-z_$]*$/gi.test(string) && !KEYWORD_REGEXP.test(string)
+}
+
+function walk(object)
+{
+  object = _filter ? _filter(object) : object
+
+  if (object === null) return 'null'
+  if (typeof object === 'undefined') return 'undefined'
+
+  var index = seen.indexOf(object);
+  if (index >= 0) return '{$circularReference:'+index+'}'
+
+  return object.toSource();
+}
+
+
+// Objects where toSource is equal to toString
+
+[Boolean, Function, Number, RegExp].forEach(function(constructor)
+{
+  var prototype = constructor.prototype
+
+  applyToSource(prototype, prototype.toString)
+});
+
+
+// Regular objects
+
+applyToSource(Date.prototype, function()
+{
+  return 'new Date('+this.getTime()+')'
+})
+
+applyToSource(String.prototype, function()
+{
+  return JSON.stringify(this)
+})
+
+applyToSource(Math, function()
+{
+  return 'Math'
+})
+
+
+// Recursive objects (Array & Object)
 
 applyToSource(Array.prototype, function()
 {
@@ -82,47 +130,15 @@ applyToSource(Object.prototype, function()
 })
 
 
-/* toSource by Marcello Bastea-Forte - zlib license */
-function walk(object)
-{
-  object = _filter ? _filter(object) : object
-
-  if (object === null) return 'null'
-  if (typeof object === 'undefined') return 'undefined'
-
-  var index = seen.indexOf(object);
-  if (index >= 0) return '{$circularReference:'+index+'}'
-
-  return object.toSource();
-}
-
-
-function getIndent(indent)
-{
-  switch(typeof indent)
-  {
-    case 'boolean':   return indent ? DEFAULT_INDENT : ''
-    case 'number':    return Array(indent+1).join(' ')
-    case 'string':    return indent
-    case 'undefined': return DEFAULT_INDENT
-  }
-
-  if(indent === null) return ''
-
-  throw SyntaxError('Invalid indent: '+indent)
-}
-
 module.exports = function(object, filter, indent)
 {
   _filter = filter
   _indent = getIndent(indent)
 
-  return walk(object)
-}
+  var result = walk(object)
 
-var KEYWORD_REGEXP = /^(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|undefined|var|void|volatile|while|with)$/
+  _filter = undefined
+  _indent = DEFAULT_INDENT
 
-function legalKey(string)
-{
-  return /^[a-z_$][0-9a-z_$]*$/gi.test(string) && !KEYWORD_REGEXP.test(string)
+  return result
 }
